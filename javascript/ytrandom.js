7f362f78a2f34bd1646ec806fb22fa42bc1a6d7c
FlameBand <flame.band.skeleton@gmail.com>
2021-08-02 14:21:45 -0500

    it didn't work

---
+++ javascript/ytrandom.js
@@ -0,0 +1,16 @@
+var youtubeLinks = [
+    "https://www.youtube.com/watch?v=aYsgsSo1aow",
+    "https://www.youtube.com/watch?v=EPNr4aztjX8",
+    "https://www.youtube.com/watch?v=SNvDUO42Hys",
+    "https://www.youtube.com/watch?v=czTksCF6X8Y",
+    "https://www.youtube.com/watch?v=8ZrQ8vh1hk8",
+    "https://www.youtube.com/watch?v=gRMNwrNk5WE",
+    "https://www.youtube.com/watch?v=gkTb9GP9lVI",
+    "https://www.youtube.com/watch?v=D-GU4jvSvz4",
+    "https://www.youtube.com/watch?v=geNMz0J9TEQ",
+    "https://www.youtube.com/watch?v=zNZ1rq5kW4M",
+    "https://www.youtube.com/watch?v=iijdWiQppq0"
+];
+
+var randomItem = youtubeLinks[Math.floor(Math.random()*youtubeLinks.length)];
+document.getElementById('trolled').href = randomItem
